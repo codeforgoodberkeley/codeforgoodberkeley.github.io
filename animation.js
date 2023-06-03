2d464194ff4a10a812c39eb15fe71f6b5c5f4bd3
Amit Chowdhary <dhruvchowdhary@berkeley.edu>
2023-06-03 13:57:45 -0500

    abstracted animation css in animation.js

--- animation.js
+++ animation.js
@@ -2,13 +2,28 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            entry.target.classList.add('show');
+            entry.target.style.opacity = '1';
+            entry.target.style.transform = 'translateY(0)';
+            entry.target.style.transition = 'all 0.8s';
+            if (entry.target.classList.contains('hidden-slow')) {
+                entry.target.style.transitionDelay = '0.1s';
+            }
         } else if (entry.boundingClientRect.top >= entry.rootBounds.top) {
-            entry.target.classList.remove('show');
+            entry.target.style.transform = 'translateY(15%)';
+            entry.target.style.opacity = '0';
         }
     });
 }, {threshold: 0.1});
 const hiddenElementsFast = document.querySelectorAll('.hidden-fast');
 const hiddenElementsSlow = document.querySelectorAll('.hidden-slow');
-hiddenElementsFast.forEach((el) => observer.observe(el));
-hiddenElementsSlow.forEach((el) => observer.observe(el));
+hiddenElementsFast.forEach((el) => {
+    observer.observe(el);
+    el.style.transform = 'translateY(15%)';
+    el.style.opacity = '0';
+});
+
+hiddenElementsSlow.forEach((el) => {
+    observer.observe(el);
+    el.style.transform = 'translateY(15%)';
+    el.style.opacity = '0';
+});
